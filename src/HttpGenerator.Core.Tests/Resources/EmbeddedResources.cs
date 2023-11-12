06955d9b97bb2c16740ce3b390d1af5c65ccb822
Christian Helle <christian.helle@outlook.com>
2023-11-12 22:58:33 +0100

    Add embedded resources utilities for HttpGenerator.Core.Tests
    This commit adds a new utility class, EmbeddedResources, in HttpGenerator.Core.Tests.Resources namespace to fetch embedded resources such as swagger petstore's json and yaml data for both V2 and V3 versions. This will be primarily used in test cases where needing to mock reading data from these resources.
    
    A new enum class SampleOpenSpecifications is also added as part of this commit, which will be used to select the specific resource version that we want to fetch using the new utility class.

---
+++ src/HttpGenerator.Core.Tests/Resources/EmbeddedResources.cs
@@ -0,0 +1,108 @@
+﻿namespace HttpGenerator.Core.Tests.Resources;
+
+public static class EmbeddedResources
+{
+    private static readonly Type Type = typeof(EmbeddedResources);
+
+    private static Stream GetStream(string name)
+        => Type.Assembly.GetManifestResourceStream(Type, name)!;
+
+    public static string SwaggerPetstoreJsonV2
+    {
+        get
+        {
+            using var stream = GetStream("V2.SwaggerPetstore.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreJsonV3
+    {
+        get
+        {
+            using var stream = GetStream("V3.SwaggerPetstore.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreYamlV2
+    {
+        get
+        {
+            using var stream = GetStream("V2.SwaggerPetstore.yaml");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreYamlV3
+    {
+        get
+        {
+            using var stream = GetStream("V3.SwaggerPetstore.yaml");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+
+    public static string SwaggerPetstoreJsonV2WithDifferentHeaders
+    {
+        get
+        {
+            using var stream = GetStream("V2.SwaggerPetstoreWithDifferentHeaders.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreJsonV3WithDifferentHeaders
+    {
+        get
+        {
+            using var stream = GetStream("V3.SwaggerPetstoreWithDifferentHeaders.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreYamlV2WithDifferentHeaders
+    {
+        get
+        {
+            using var stream = GetStream("V2.SwaggerPetstoreWithDifferentHeaders.yaml");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreYamlV3WithDifferentHeaders
+    {
+        get
+        {
+            using var stream = GetStream("V3.SwaggerPetstoreWithDifferentHeaders.yaml");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+
+
+    public static string GetSwaggerPetstore(SampleOpenSpecifications version)
+    {
+        return version switch
+        {
+            SampleOpenSpecifications.SwaggerPetstoreJsonV2 => SwaggerPetstoreJsonV2,
+            SampleOpenSpecifications.SwaggerPetstoreJsonV3 => SwaggerPetstoreJsonV3,
+            SampleOpenSpecifications.SwaggerPetstoreYamlV2 => SwaggerPetstoreYamlV2,
+            SampleOpenSpecifications.SwaggerPetstoreYamlV3 => SwaggerPetstoreYamlV3,
+            SampleOpenSpecifications.SwaggerPetstoreJsonV2WithDifferentHeaders => SwaggerPetstoreJsonV2WithDifferentHeaders,
+            SampleOpenSpecifications.SwaggerPetstoreJsonV3WithDifferentHeaders => SwaggerPetstoreJsonV3WithDifferentHeaders,
+            SampleOpenSpecifications.SwaggerPetstoreYamlV2WithDifferentHeaders => SwaggerPetstoreYamlV2WithDifferentHeaders,
+            SampleOpenSpecifications.SwaggerPetstoreYamlV3WithDifferentHeaders => SwaggerPetstoreYamlV3WithDifferentHeaders,
+            _ => throw new ArgumentOutOfRangeException(nameof(version), version, null)
+        };
+    }
+}
---
+++ src/HttpGenerator.Core.Tests/Resources/SampleOpenSpecifications.cs
@@ -0,0 +1,13 @@
+﻿namespace HttpGenerator.Core.Tests.Resources;
+
+public enum SampleOpenSpecifications
+{
+    SwaggerPetstoreJsonV2,
+    SwaggerPetstoreJsonV3,
+    SwaggerPetstoreYamlV2,
+    SwaggerPetstoreYamlV3,
+    SwaggerPetstoreJsonV2WithDifferentHeaders,
+    SwaggerPetstoreJsonV3WithDifferentHeaders,
+    SwaggerPetstoreYamlV2WithDifferentHeaders,
+    SwaggerPetstoreYamlV3WithDifferentHeaders,     
+}
