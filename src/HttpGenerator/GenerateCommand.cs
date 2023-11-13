4163167dfd651068b7f7d0f2e8bbb3d02845a124
Christian Helle <christian.helle@outlook.com>
2023-11-13 15:26:11 +0100

    Add HTTP file generator and command-line application
    This commit includes the creation of the HttpGenerator feature which interacts with OpenAPI specifications to generate HTTP client files. It also adds a command line application to interface with the generator for convenient usage. Encompassed is also code to handle logging and exception handling, providing user feedback during operation execution.

---
+++ src/HttpGenerator/GenerateCommand.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using HttpGenerator.Core;
+using HttpGenerator.Validation;
+using Microsoft.OpenApi.Models;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace HttpGenerator;
+
+public class GenerateCommand : AsyncCommand<Settings>
+{
+    private static readonly string Crlf = Environment.NewLine;
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        try
+        {
+            var stopwatch = Stopwatch.StartNew();
+            AnsiConsole.MarkupLine($"[green]HTTP File Generator v{GetType().Assembly.GetName().Version!}[/]");
+            AnsiConsole.MarkupLine(
+                settings.NoLogging
+                    ? "[green]Support key: Unavailable when logging is disabled[/]"
+                    : $"[green]Support key: {SupportInformation.GetSupportKey()}[/]");
+            
+            await ValidateOpenApiSpec(settings);
+            var result = await HttpFileGenerator.Generate(settings.OpenApiPath!);
+
+            var directory = Path.GetDirectoryName(settings.OutputFolder);
+            if (!string.IsNullOrWhiteSpace(directory) && !Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            foreach (var file in result.Files)
+            {
+                var outputFile = Path.Combine(directory!, file.Filename);
+                await File.WriteAllTextAsync(outputFile, file.Content);
+            }
+
+            AnsiConsole.MarkupLine($"[green]Files: {result.Files.Count}[/]");
+            AnsiConsole.MarkupLine($"[green]Duration: {stopwatch.Elapsed}{Crlf}[/]");
+            return 0;
+        }
+        catch (Exception exception)
+        {
+            if (exception is not OpenApiValidationException)
+            {
+                AnsiConsole.MarkupLine($"[red]Error:{Crlf}{exception.Message}[/]");
+                AnsiConsole.MarkupLine($"[red]Exception:{Crlf}{exception.GetType()}[/]");
+                AnsiConsole.MarkupLine($"[yellow]Stack Trace:{Crlf}{exception.StackTrace}[/]");
+            }
+
+            await Analytics.LogError(exception, settings);
+            return exception.HResult;
+        }
+    }
+
+    private static async Task ValidateOpenApiSpec(Settings settings)
+    {
+        var validationResult = await OpenApiValidator.Validate(settings.OpenApiPath!);
+        if (!validationResult.IsValid)
+        {
+            AnsiConsole.MarkupLine($"[red]{Crlf}OpenAPI validation failed:{Crlf}[/]");
+
+            foreach (var error in validationResult.Diagnostics.Errors)
+            {
+                TryWriteLine(error, "red", "Error");
+            }
+
+            foreach (var warning in validationResult.Diagnostics.Warnings)
+            {
+                TryWriteLine(warning, "yellow", "Warning");
+            }
+
+            validationResult.ThrowIfInvalid();
+        }
+
+        AnsiConsole.MarkupLine($"[green]{Crlf}OpenAPI statistics:{Crlf}{validationResult.Statistics}{Crlf}[/]");
+    }
+
+    private static void TryWriteLine(
+        OpenApiError error,
+        string color,
+        string label)
+    {
+        try
+        {
+            AnsiConsole.MarkupLine($"[{color}]{label}:{Crlf}{error}{Crlf}[/]");
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+}
---
+++ src/HttpGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Spectre.Console.Cli;
+
+namespace HttpGenerator;
+
+[ExcludeFromCodeCoverage]
+internal static class Program
+{
+    private static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            args = new[]
+            {
+                "--help"
+            };
+        }
+
+        var app = new CommandApp<GenerateCommand>();
+        app.Configure(
+            configuration =>
+            {
+                configuration
+                    .SetApplicationName("httpgenerator")
+                    .SetApplicationVersion(typeof(GenerateCommand).Assembly.GetName().Version!.ToString());
+
+                configuration
+                    .AddExample("./openapi.json");
+
+                configuration
+                    .AddExample("https://petstore3.swagger.io/api/v3/openapi.yaml");
+
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--output",
+                        "./");
+            });
+
+        return app.Run(args);
+    }
+}
