31a1433e119d9be26f4389671fbd4f021aa1f0e4
Christian Helle <christian.helle@outlook.com>
2023-11-13 11:32:42 +0100

    Add GeneratorSettings in HttpGenerator.Core
    Introduced a new class, GeneratorSettings, in HttpGenerator.Core which provides settings for the .http file generator. This class includes a constant for default output folder and a property to fetch or set the path to the Open API. This addition will allow more configurable and versatile utilisation of the .http file generator.

---
+++ src/HttpGenerator.Core/GeneratorSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HttpGenerator.Core;
+
+/// <summary>
+/// Provide settings for the .http file generator.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class GeneratorSettings
+{
+    public const string DefaultOutputFolder = "./Generated";
+
+    /// <summary>
+    /// Gets or sets the path to the Open API (local file or URL)
+    /// </summary>
+    public string OpenApiPath { get; set; } = null!;
+}
