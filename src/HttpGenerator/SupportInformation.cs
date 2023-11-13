ad2710d5e8058963aacc12f526192e3a8dc3399f
Christian Helle <christian.helle@outlook.com>
2023-11-13 15:23:39 +0100

    Add SupportInformation class for user identity hashing
    A new class, SupportInformation, has been introduced into the HttpGenerator namespace to secure sensitive user and machine information. This class provides utility methods to generate an anonymous support key and hashed identity information from the user's username and machine name. This enhances the security of user identity data in the system by avoiding plain text user and machine name exposure.

---
+++ src/HttpGenerator/SupportInformation.cs
@@ -0,0 +1,32 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HttpGenerator;
+
+public static class SupportInformation
+{
+    public static string GetSupportKey()
+        => GetAnonymousIdentity()[..7];
+
+    public static string GetAnonymousIdentity()
+        => $"{Environment.UserName}@{GetMachineName()}".ToSha256();
+
+    private static string GetMachineName()
+    {
+        try
+        {
+            return Environment.MachineName;
+        }
+        catch
+        {
+            return "localhost";
+        }
+    }
+
+    private static string ToSha256(this string value)
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+        var hash = SHA256.HashData(bytes);
+        return Convert.ToBase64String(hash).ToLowerInvariant();
+    }
+}
