ec10e808f008bef8ba1d2e62fa6d18bc07287838
Christian Helle <christian.helle@outlook.com>
2023-11-13 15:25:09 +0100

    Add Analytics.cs for logging and error reporting
    A new file, Analytics.cs, has been added to the HttpGenerator project. This file is responsible for setting up and utilizing the Exceptionless library for logging and error reporting. Features include configuring Exceptionless client, logging feature usage, and logging exceptions. This addition will improve monitoring, debugging, and understanding of application usage and errors.

---
+++ src/HttpGenerator/Analytics.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+using Exceptionless;
+using Exceptionless.Plugins;
+using Spectre.Console.Cli;
+using HttpGenerator.Core;
+
+namespace HttpGenerator;
+
+public static class Analytics
+{
+    public static void Configure()
+    {
+        ExceptionlessClient.Default.Configuration.SetUserIdentity(
+            SupportInformation.GetAnonymousIdentity(),
+            SupportInformation.GetSupportKey());
+
+        ExceptionlessClient.Default.Configuration.UseSessions();
+        ExceptionlessClient.Default.Configuration.SetVersion(typeof(GenerateCommand).Assembly.GetName().Version!);
+        ExceptionlessClient.Default.Startup("7VSRHLYiJdF7Xp0WaVwmEbJxVmrjqHnTIZNKkrkI");
+    }
+
+    public static Task LogFeatureUsage(Settings settings)
+    {
+        if (settings.NoLogging)
+            return Task.CompletedTask;
+
+        foreach (var property in typeof(Settings).GetProperties())
+        {
+            if (!CanLogFeature(settings, property))
+            {
+                continue;
+            }
+
+            property.GetCustomAttributes(typeof(CommandOptionAttribute), true)
+                .OfType<CommandOptionAttribute>()
+                .Where(
+                    attribute =>
+                        !attribute.LongNames.Contains("output") &&
+                        !attribute.LongNames.Contains("no-logging"))
+                .ToList()
+                .ForEach(
+                    attribute =>
+                        ExceptionlessClient.Default
+                            .CreateFeatureUsage(attribute.LongNames.FirstOrDefault() ?? property.Name)
+                            .Submit());
+        }
+
+        return ExceptionlessClient.Default.ProcessQueueAsync();
+    }
+
+    private static bool CanLogFeature(Settings settings, PropertyInfo property)
+    {
+        var value = property.GetValue(settings);
+        if (value is null or false)
+            return false;
+
+        if (property.PropertyType == typeof(string[]) && ((string[])value).Length == 0)
+            return false;
+
+        return true;
+    }
+
+    public static Task LogError(Exception exception, Settings settings)
+    {
+        if (settings.NoLogging)
+            return Task.CompletedTask;
+
+        exception
+            .ToExceptionless(
+                new ContextData(
+                    Serializer.Deserialize<Dictionary<string, object>>(
+                        Serializer.Serialize(settings))!))
+            .Submit();
+
+        return ExceptionlessClient.Default.ProcessQueueAsync();
+    }
+}
