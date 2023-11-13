2f185eba8a4684c6ae43a6b8cb47fa8559265598
Christian Helle <christian.helle@outlook.com>
2023-11-13 14:08:04 +0100

    Add SwaggerPetstore tests to HttpGenerator
    This commit introduces the class `SwaggerPetstoreTests` to the HttpGenerator.Core.Tests namespace. This test class includes methods for generating code and building it from the URL for different versions of the Swagger Petstore API (both JSON and YAML). These tests help ensure that the HttpGenerator correctly handles different API versions and specifications.

---
+++ src/HttpGenerator.Core.Tests/SwaggerPetstoreTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using HttpGenerator.Core.Tests.Resources;
+
+namespace HttpGenerator.Core.Tests;
+
+public class SwaggerPetstoreTests
+{
+    [Theory]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Can_Generate_Code(SampleOpenSpecifications version, string filename)
+    {
+        var generateCode = await GenerateCode(version, filename);
+        generateCode.Should().NotBeNull();
+        generateCode.Files.Should().NotBeNullOrEmpty();
+    }
+
+    [Theory]
+    [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
+    [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.yaml")]
+    [InlineData("https://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
+    [InlineData("https://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.yaml")]
+    public async Task Can_Build_Generated_Code_From_Url(string url)
+    {
+        var generateCode = await HttpFileGenerator.Generate(url);
+        generateCode.Should().NotBeNull();
+        generateCode.Files.Should().NotBeNullOrEmpty();
+    }
+
+    private static async Task<GeneratorResult> GenerateCode(
+        SampleOpenSpecifications version,
+        string filename)
+    {
+        var swaggerFile = await TestFile.CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
+        return await HttpFileGenerator.Generate(swaggerFile);
+    }
+}
