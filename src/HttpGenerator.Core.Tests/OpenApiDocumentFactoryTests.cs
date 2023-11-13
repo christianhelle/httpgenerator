9fdc80d5086f85e6fcdd66b0b44ba4ec2c1e897f
Christian Helle <christian.helle@outlook.com>
2023-11-13 11:26:28 +0100

    Add OpenApiDocumentFactory tests
    A new test file named OpenApiDocumentFactoryTests.cs has been created. It contains unit tests for checking if the `OpenApiDocumentFactory` methods can successfully create OpenAPI documents from both URLs and files, ensuring no null responses. These tests are important to maintain the quality and reliability of our document generation from various sources.

---
+++ src/HttpGenerator.Core.Tests/OpenApiDocumentFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HttpGenerator.Core.Tests.Resources;
+
+namespace HttpGenerator.Core.Tests;
+
+public class OpenApiDocumentFactoryTests
+{
+    [Theory]
+    [InlineData("https://developers.intellihr.io/docs/v1/swagger.json")] // GZIP encoded
+    [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
+    public async Task Create_From_Uri_Returns_NotNull(string url)
+    {
+        (await OpenApiDocumentFactory.CreateAsync(url))
+            .Should()
+            .NotBeNull();
+    }
+    
+    [Theory]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Create_From_File_Returns_NotNull(SampleOpenSpecifications version, string filename)
+    {
+        var swaggerFile = await TestFile.CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
+        (await OpenApiDocumentFactory.CreateAsync(swaggerFile))
+            .Should()
+            .NotBeNull();
+    }
+}
