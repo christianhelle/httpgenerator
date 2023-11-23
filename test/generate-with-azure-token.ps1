10f302630cd40a5eddd2fc38dccd8445dd8c4af1
Christian Helle <christian.helle@outlook.com>
2023-11-23 10:07:08 +0100

    Add script to generate HTTP files with Azure token
    The script 'generate-with-azure-token.ps1' has been added to ease the HTTP files' generation process by using Azure tokens. This script will authenticate using Azure tokens, generating HTTP files from the petstore3 swagger API specification. The goal is to automate and secure the generation process.

---
+++ test/generate-with-azure-token.ps1
@@ -0,0 +1,9 @@
+az account get-access-token `
+| ConvertFrom-Json `
+| %{
+    dotnet run --project ../src/HttpGenerator/HttpGenerator.csproj -- `
+    https://petstore3.swagger.io/api/v3/openapi.json `
+        --authorization-header ("Bearer " + $_.accessToken) `
+        --base-url https://petstore3.swagger.io/ `
+        --output ./HttpFiles 
+}
