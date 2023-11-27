891f333e40ccee6133ec1b0d1de3e5232df4c4af
Christian Helle <christian.helle@outlook.com>
2023-11-27 14:10:21 +0100

    Remove unused ConvertKebabCaseToCamelCase from StringExtensions
    The ConvertKebabCaseToCamelCase function was removed from the StringExtensions.cs file. This function was deemed unnecessary as kebab-case can be processed elsewhere in the application, or no longer needed given recent changes to the application structure and function requirements.

--- src/HttpGenerator.Core/StringExtensions.cs
+++ src/HttpGenerator.Core/StringExtensions.cs
@@ -16,17 +16,6 @@ public static class StringExtensions
         return string.Join(string.Empty, parts);
     }
 
-    public static string ConvertKebabCaseToCamelCase(this string str)
-    {
-        var parts = str.Split('-');
-        for (var i = 1; i < parts.Length; i++)
-        {
-            parts[i] = parts[i].CapitalizeFirstCharacter();
-        }
-
-        return string.Join(string.Empty, parts);
-    }
-
     public static string ConvertRouteToCamelCase(this string str)
     {
         var parts = str.Split('/');
