6417256a3bb9d8c0ef9295e949a47c7cc0ad2dda
Christian Helle <christian.helle@outlook.com>
2023-12-12 23:06:59 +0100

    Add HttpGenerator.VSIX; implement 'Generate .http files' command
    HttpGenerator.VSIX has been incorporated as a Visual Studio extension within the main solution. It now includes the 'Generate .http files' command that generates .http files from OpenAPI (Swagger) specifications. This addition encompasses project files delineating project structure/properties, command definitions, .vsixmanifest rules, and a new VSIX solution file named VSIX.sln.

---
+++ src/HttpGenerator.VSIX/GenerateTestPlansCommand.cs
@@ -0,0 +1,116 @@
+﻿using Community.VisualStudio.Toolkit;
+using HttpGenerator.Core;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace HttpGenerator.VSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GenerateTestPlansCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7A2B56F6-08C0-4124-854D-3CBD96A41190");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateTestPlansCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private GenerateTestPlansCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GenerateTestPlansCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in GenerateTestPlansCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new GenerateTestPlansCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void Execute(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var project = await VS.Solutions.GetActiveProjectAsync();
+
+            var generatorSettings = new GeneratorSettings
+            {
+                OpenApiPath = "https://petstore3.swagger.io/api/v3/openapi.json"
+            };
+
+            var result = await HttpFileGenerator.Generate(generatorSettings);
+
+            var output = Path.GetDirectoryName(project.FullPath);
+            output = Path.Combine(output, "HttpFiles");
+            if (!Directory.Exists(output))
+            {
+                Directory.CreateDirectory(output);
+            }
+
+            Parallel.ForEach(
+                result.Files,
+                file => File.WriteAllText(
+                    Path.Combine(output, file.Filename),
+                    file.Content));
+        }
+    }
+}
