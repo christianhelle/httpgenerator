735eeae6267965fecc4f601cc702cac3d112d635
Christian Helle <christian.helle@outlook.com>
2023-12-20 15:11:04 +0100

    Add convenience class for retrieving Azure Entra ID access token for a given scope and tenant

---
+++ src/HttpGenerator.Core/AzureEntraID.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Core;
+using Azure.Identity;
+
+namespace HttpGenerator.Core
+{
+    public static class AzureEntraID
+    {
+        public static async Task<string?> TryGetAccessTokenAsync(
+            string tenantId,
+            string scope,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var request = new TokenRequestContext([scope], tenantId: tenantId);
+                var credentials = new DefaultAzureCredential(
+                    new DefaultAzureCredentialOptions
+                    {
+                        ExcludeEnvironmentCredential = true,
+                        ExcludeWorkloadIdentityCredential = true,
+                        ExcludeManagedIdentityCredential = true,
+                    });
+                var token = await credentials.GetTokenAsync(request, cancellationToken);
+                return token.Token;
+            }
+            catch (OperationCanceledException)
+            {
+                // Ignore
+            }
+
+            return null;
+        }
+    }
+}
