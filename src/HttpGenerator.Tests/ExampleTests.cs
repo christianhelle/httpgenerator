cd518ad353bee7495be3577ef8286ed13e91248a
Christian Helle <christian.helle@outlook.com>
2025-10-20 22:24:45 +0200

    Add ExampleTests to verify code generation output

---
+++ src/HttpGenerator.Tests/ExampleTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using HttpGenerator.Core;
+
+namespace HttpGenerator.Tests;
+
+public class ExampleTests
+{
+    [Theory]
+    [InlineData("https://demo.netbox.dev/api/schema", OutputType.OneFile)]
+    [InlineData("https://demo.netbox.dev/api/schema", OutputType.OneFilePerTag)]
+    [InlineData("https://demo.netbox.dev/api/schema", OutputType.OneRequestPerFile)]
+    public async Task Should_Return_Valid_Code(string url, OutputType outputType)
+    {
+        var generateCode = await HttpFileGenerator.Generate(
+            new()
+            {
+                OpenApiPath = url,
+                OutputType = outputType,
+                GenerateIntelliJTests = true,
+            });
+
+        using var scope = new AssertionScope();
+        generateCode.Should().NotBeNull();
+        generateCode.Files.Should().NotBeNullOrEmpty();
+    }
+}
