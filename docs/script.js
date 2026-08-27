0848ae10a3fceca75f1f0d9c11e93ffcb1d3a9ac
Christian Helle <christian.helle@outlook.com>
2026-08-27 14:08:38 +0200

    use at() to fetch last performance entry

--- docs/script.js
+++ docs/script.js
@@ -175,7 +175,7 @@ class PerformanceMonitor {
         if ('PerformanceObserver' in window) {
             new PerformanceObserver((entryList) => {
                 const entries = entryList.getEntries();
-                const lastEntry = entries[entries.length - 1];
+                const lastEntry = entries.at(-1);
                 console.log('LCP:', lastEntry.startTime);
             }).observe({ entryTypes: ['largest-contentful-paint'] });
         }
